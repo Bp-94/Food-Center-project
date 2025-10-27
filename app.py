828578d32993af69bd17459227df2d7a788b1991
Bp-94 <Beampanithi49@gmail.com>
2025-10-27 23:49:32 +0700

    test page finish

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def menu():
     return render_template("main_menu.html")
+
+@app.route('/income')
+def income():
+    return render_template("income.html")
+
 if __name__ == "__main__":
     app.run()
