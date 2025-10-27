23f0b3314ffe07d0e100fadbaeeaea4a18a525ac
Bp-94 <Beampanithi49@gmail.com>
2025-10-27 23:18:17 +0700

    add main_menu

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask ,render_template
 app = Flask(__name__)
 
 if __name__ == "__main__":
     app.run()
+@app.route('/')
+def menu():
+    return render_template("main_menu.html")
