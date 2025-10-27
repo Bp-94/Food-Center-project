191e8f1920981cb116182793a96b965ecc2d43c7
Bp-94 <Beampanithi49@gmail.com>
2025-10-27 23:23:04 +0700

    test route mainmenu

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask ,render_template
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run()
 @app.route('/')
 def menu():
     return render_template("main_menu.html")
+if __name__ == "__main__":
+    app.run()
