866e6ed083930fa5074be249c457d9a7225527e4
uuxianzi <68070240@kmitl.ac.th>
2025-11-02 14:56:43 +0700

    customer

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/noodle')
+def noodle_page():
+    return render_template('noodle.html')
+
+@app.route('/pay')
+def pay_page():
+    return render_template('pay.html')
+
+@app.route('/order')
+def order_page():
+    return render_template('order.html')
+
+@app.route('/account')
+def account_page():
+    return render_template('account.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
