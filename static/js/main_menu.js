26e9878881739e6fc3328869f799c51a9d7c7bde
Naphak7532 <68070274@kmitl.ac.th>
2025-11-04 04:11:20 +0700

    123

--- static/js/main_menu.js
+++
@@ -1,4 +0,0 @@
-button_menu = document.querySelector('menu')
-button_revenue = document.querySelector('revenue')
-button_order = document.querySelector('order')
-button_payment = document.querySelector('payment')
--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-# TEGER PRIMARY KEY AUTOINCREMENT,
-#              seller_id INTEGER NOT NULL,
-#              name_menu TEXT NOT NULL,
-#              price REAL NOT NULL,
-#              description TEXT NOT NULL
-#              )
