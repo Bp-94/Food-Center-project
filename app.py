703759a5a3fa210fa1f1e59cb3125fa86c711959
Bp-94 <Beampanithi49@gmail.com>
2025-11-06 12:44:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os , uuid
+#import os , uuid
 from flask import Flask, render_template,request,redirect, url_for,session
 import sqlite3
 from werkzeug.utils import secure_filename
@@ -404,4 +404,4 @@ def pay_confirm():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
-    app.run(host='0.0.0.0, port=port)
+    app.run(host='0.0.0.0', port=port)
